b8753ba0e7c058106c5b3cbc19d95c95b16e18ec
dkGithup2022 <104286091+dkGithup2022@users.noreply.github.com>
2023-04-02 20:46:07 +0900

    Feat: 의존성 다운로드

---
+++ pulling_app/src/main/java/com/dk0124/cdr/pullapp/App.java
@@ -0,0 +1,11 @@
+package com.dk0124.cdr.pullapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
